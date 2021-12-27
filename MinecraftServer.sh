ffd25fc01d7e4ca6e135f0dd911ede0aea2a4f67
imaspacecat <72170757+imaspacecat@users.noreply.github.com>
2021-12-27 03:02:55 -0800

    Update MinecraftServer.sh
    comments

--- MinecraftServer.sh
+++ MinecraftServer.sh
@@ -1,16 +1,16 @@
 #! /usr/bin/bash
 #install Java 17 (required for 1.18+ servers)
-#get the file
 cd $HOME/Downloads
 Red=$'\e[1;31m'
+#get the file
 wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.1%2B12/OpenJDK17U-jdk_arm_linux_hotspot_17.0.1_12.tar.gz
-#extract it
 echo "$Red Extracting..."
+#extract it
 tar xzf OpenJDK17U-jdk_arm_linux_hotspot_17.0.1_12.tar.gz
 #move the extracted files to /usr/lib/jvm
 sudo mv jdk-17.0.1+12 /usr/lib/jvm/
 cd /usr/lib/jvm
-#add java to the update alternatives so you can change to other versions later if desired
+#add java version to the update alternatives so you can change to other versions later if desired
 sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk-16.0.2+7/bin/java 1131
 echo "$Red Enter the number corresponding to Java 17"
 sudo update-alternatives --config java
@@ -21,5 +21,6 @@ cd MinecraftServer
 #install the 1.18.1 server.jar from the Minecraft Website
 wget https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar
 java -Xmx1024M -Xms1024M -jar server.jar nogui
+#sign eula (you must to run the server)
 sed -i "s/false/true/" eula.txt
 java -Xmx1024M -Xms1024M -jar server.jar nogui
